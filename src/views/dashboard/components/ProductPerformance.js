8e607390e378cef51b7e17035da73179b5b63444
Akshay Waghmare <akshaywaghmare@Akshays-MacBook-Air.local>
2024-06-01 02:05:02 +0530

    migrated

---
+++ src/views/dashboard/components/ProductPerformance.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import {
+    Typography, Box,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Chip
+} from '@mui/material';
+import DashboardCard from '../../../components/shared/DashboardCard';
+
+const products = [
+    {
+        id: "1",
+        name: "akshay@wemofy.in",
+        post: "support@wemofy.in",
+        pname: " test 1 - from the email",
+        priority: "Low",
+        pbg: "primary.main",
+        Time: "22:04 , Friday",
+    },
+    {
+        id: "2",
+        name: "akshay@wemofy.in",
+        post: "support@wemofy.in",
+        pname: " test 1 - from the email",
+        priority: "Low",
+        pbg: "primary.main",
+        budget: "22:04 , Friday",
+    },
+    {
+        id: "3",
+        name: "akshay@wemofy.in",
+        post: "support@wemofy.in",
+        pname: " test 1 - from the email",
+        priority: "Low",
+        pbg: "primary.main",
+        budget: "22:04 , Friday",
+    },
+    {
+        id: "4",
+        name: "nitio@gmail.com ",
+        post: "support@wemofy.in",
+        pname: "Hosting Press HTML",
+        priority: "Critical",
+        pbg: "success.main",
+        budget: "2.4",
+    },
+];
+
+
+const ProductPerformance = () => {
+    return (
+
+        <DashboardCard title="Open Tickets">
+            <Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
+                <Table
+                    aria-label="simple table"
+                    sx={{
+                        whiteSpace: "nowrap",
+                        mt: 2
+                    }}
+                >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Id
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Assigned
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Name
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Priority
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Check Details
+                                </Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {products.map((product) => (
+                            <TableRow key={product.name}>
+                                <TableCell>
+                                    <Typography
+                                        sx={{
+                                            fontSize: "15px",
+                                            fontWeight: "500",
+                                        }}
+                                    >
+                                        {product.id}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                        }}
+                                    >
+                                        <Box>
+                                            <Typography variant="subtitle2" fontWeight={600}>
+                                                {product.name}
+                                            </Typography>
+                                            <Typography
+                                                color="textSecondary"
+                                                sx={{
+                                                    fontSize: "13px",
+                                                }}
+                                            >
+                                                {product.post}
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography color="textSecondary" variant="subtitle2" fontWeight={400}>
+                                        {product.pname}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Chip
+                                        sx={{
+                                            px: "4px",
+                                            backgroundColor: product.pbg,
+                                            color: "#fff",
+                                        }}
+                                        size="small"
+                                        label={product.priority}
+                                    ></Chip>
+                                </TableCell>
+                                <TableCell align="right">
+                                    <Typography variant="h6">${product.budget}k</Typography>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Box>
+        </DashboardCard>
+    );
+};
+
+export default ProductPerformance;
